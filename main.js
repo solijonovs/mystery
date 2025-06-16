1ba760c50c119554a1d4bfb74d90a0935dd73720
solijonovs@berea.edu <solijonovs@berea.edu>
2025-06-16 13:00:06 -0400

    done!!!

--- main.js
+++ main.js
@@ -13,6 +13,68 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// Below test the mockUpStrand function that uses a helper function of returnRandBase
+// console.log(mockUpStrand());
+
+
+//Factory function that returns an object contains the propersties corresponding to the parameters provided
+const pAequorFactory = (specimenNum, dna) => {
+    return {
+      _specimenNum: specimenNum,
+      _dna: dna,
+
+      mutate() {
+        let randomIndex = Math.floor(Math.random() * this._dna.length);
+        let newBase = returnRandBase();
+        while(this._dna[randomIndex] === newBase) {
+          newBase = returnRandBase();
+        }
+        this._dna = newBase;
+        return this._dna;
+      },
+      compareDNA(arr) {
+        let identicalBases = 0 ;
+        for(let i = 0; i < arr.length; i++) {
+          if(this._dna === arr.dna[i]) {
+            identicalBases++;
+          }
+        }
+        const percentage = ((identicalBases/this._dna.length) * 100).toFixed(2);
+        console.log(`specimen #${this.specimenNum} and specimen #${dna.specimenNum} have ${percentage}% DNA in common!`);
+      },
+      willLikelySurvive() {
+        let identicalBases = 0;
+        for(let i = 0; i < this._dna.length; i++) {
+          if(this._dna[i] === 'C' || this._dna[i] === 'G') {
+            identicalBases++;
+          }
+        }
+        const percentage = ((identicalBases/this._dna.length) * 100);
+        if(percentage > 60) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+  }
+}
+
+const survivingPAqueor = [];
+let id = 1;
+while(survivingPAqueor.length < 30) {
+  const newOrganism = pAequorFactory(id, mockUpStrand());
+  if(newOrganism.willLikelySurvive()) {
+    survivingPAqueor.push(newOrganism);
+  }
+  id++
+}
+
+console.log(survivingPAqueor);
+
+
+
+
 
 
 
--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -13,6 +13,78 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+//Task 3 
+function pAequorFactory(dna, specimenNum) {
+  return {
+    dna,
+    specimenNum,
+
+    mutate() {
+      let randomIndex = Math.floor(Math.random() * this.dna.length);
+      let randomBase = returnRandBase();
+      while(this.dna[randomIndex] === randomBase) {
+        randomBase = returnRandBase();
+      }
+      this.dna[randomIndex] = randomBase;
+      return this.dna;
+    },
+
+    compareDNA(pAequor) {
+      let identicalBases = 0;
+      for(let i = 0; i < this.dna.length; i++) {
+        if(this.dna[i] === pAequor.dna[i]) {
+          identicalBases++;
+        }
+      }
+      const percentage = ((identicalBases / this.dna.length) * 100).toFixed(2);
+      console.log(`The specimen ${this.specimenNum} and ${pAequor.specimenNum} have ${percentage} % in common.`);
+    },
+
+    willLikelySurvive() {
+      let filtered = this.dna.filter((value) => value === 'C' || value === 'G')
+      let percentage = ((filtered.length / this.dna.length) * 100);
+      if(percentage > 60) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+const survivingPAqueor = [];
+let id = 1;
+while(survivingPAqueor.length < 30) {
+  let pAequor = pAequorFactory(mockUpStrand(), id);
+  if(pAequor.willLikelySurvive()) {
+    survivingPAqueor.push(pAequor); 
+    id++;
+  } 
+}
+
+
+//Testing for Task 3 (Creating the instance of pAequor of test1 and test2)
+//let test = pAequorFactory(mockUpStrand(), 2)
+//let test2 = pAequorFactory(mockUpStrand(), 4);
+
+//Testing for Task 4
+//console.log(test);
+//console.log(test.mutate());
+
+//Test for Task 5
+//console.log(test);
+//console.log(test2);
+//console.log(test.compareDNA(test2));
+
+//Test for Task 6
+//console.log(test.dna);
+//console.log(test.willLikelySurvive());
+
+//Test for Task 7
+console.log(survivingPAqueor);
+
+
+
 
 
 
